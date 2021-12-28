fe55cd6b5f0c38c0b6de98588bab86acdf8eb79f
Camilo <cavs1010@gmail.com>
2021-12-29 06:30:49 +1000

    branching

---
+++ src/Components/App/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+          <div>
+              <h1>Ja<span class="highlight">mmm</span>ing</h1>
+              <div class="App">
+              {/**<!-- Add a SearchBar component -->**/}
+              <h2>Search Component</h2>
+                <div class="App-playlist">
+                  {/**<!-- Add a SearchResults component -->
+                  <!-- Add a Playlist component -->**/}
+                  <h2>Search Results</h2>
+                  <h2>Playlist component</h2>
+                </div>
+              </div>
+            </div>
+  );
+}
+
+export default App;
