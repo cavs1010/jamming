b7f292d5c7e9b8e7f5b763dcd7b19f3b0b2b561d
Camilo <cavs1010@gmail.com>
2021-12-29 07:00:05 +1000

    Search Bar and Search Results

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,16 +1,20 @@
 import './App.css';
+import { SearchBar } from '../SearchBar/SearchBar';
+import { SearchResults } from '../SearchResults/SearchResults'
 
 function App() {
   return (
           <div>
-              <h1>Ja<span class="highlight">mmm</span>ing</h1>
-              <div class="App">
+              <h1>Ja<span className="highlight">mmm</span>ing</h1>
+              <div className="App">
               {/**<!-- Add a SearchBar component -->**/}
+              <SearchBar />
               <h2>Search Component</h2>
-                <div class="App-playlist">
+                <div className="App-playlist">
                   {/**<!-- Add a SearchResults component -->
                   <!-- Add a Playlist component -->**/}
                   <h2>Search Results</h2>
+                  <SearchResults />
                   <h2>Playlist component</h2>
                 </div>
               </div>
---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,14 @@
+import './SearchResults.css'
+import React from 'react';
+import PlayList from '../Playlist/Playlist'
+
+export class SearchResults extends React.Component{
+	render(){
+		return(
+			<div className="SearchResults">
+				<h2>Results</h2>
+				{/**<!-- Add a TrackList component -->*/}
+			</div>
+		)
+	}
+} 	
